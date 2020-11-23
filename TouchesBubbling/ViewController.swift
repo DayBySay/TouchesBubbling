40ea01701acb0ba7927706cefe26ef7e8febf42d
daybysay <yukita.sekai+github@gmail.com>
2020-11-23 23:59:55 +0900

    hittest周りの実装追加

--- TouchesBubbling/ViewController.swift
+++ TouchesBubbling/ViewController.swift
@@ -17,3 +17,42 @@ class ViewController: UIViewController {
     }
 }
 
+class PrintHitTestView: UIView {
+    @IBInspectable var name: String = ""
+    
+    override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        print("hit test!: \(point), view name: \(name)")
+        return super.hitTest(point, with: event)
+    }
+    
+    override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
+        let result = super.point(inside: point, with: event)
+        print("point!: \(point), boolean: \(result) view name: \(name)")
+        return result
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("touched began!: \(touches),\n vew name: \(name)")
+        super.touchesBegan(touches, with: event)
+    }
+}
+
+class CustomWKWebView: WKWebView {
+    @IBInspectable var name: String = ""
+    
+    override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        print("hit test!: \(point), view name: \(name)")
+        return super.hitTest(point, with: event)
+    }
+    
+    override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
+        let result = super.point(inside: point, with: event)
+        print("point!: \(point), boolean: \(result) view name: \(name)")
+        return result
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("touched began!: \(touches),\n vew name: \(name)")
+        super.touchesBegan(touches, with: event)
+    }
+}
